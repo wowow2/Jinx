caaf9cc1b4c1870fd88dcf9a25fd57ee9e652399
Leigh <jcarleton537@outlook.com>
2024-02-21 14:29:50 -0700

    adding .txt

--- Jinx.py
+++ Jinx.py
@@ -1,3 +1,4 @@
+'''
 while True:
     FavChar = input("Who is your fav Arcane character?")
     FavChar = FavChar.upper()
@@ -16,3 +17,12 @@ while True:
 #meow test test #3
 
 #meow test test #4
+'''
+
+# Starting the code
+with open('Arcane-transcript-104-Happy-Progress-Day.txt', 'r') as EpisodeOne:
+  EpOneLines = EpisodeOne.readlines()
+  JinxEpOne = []
+  for EpisodeOne in EpOneLines:
+    Line = BLANK.strip().split(",")#for here code to make lines that dont start with jinx go away
+    JinxEpOne.append(Line)

3dbfe80b65e8502209affc8357d4569ddd150435
Leigh <jcarleton537@outlook.com>
2024-02-21 13:43:42 -0700

    hello

---
+++ Jinx.py
@@ -0,0 +1,13 @@
+
+while True:
+    FavChar = input("Who is your fav Arcane character?").upper
+    if FavChar == "JINX":
+        print("That's mine too")
+        break
+    elif FavChar == "CAITLYN" or FavChar = "VI":
+        print("Solid choice! ")
+        break
+    else:
+        print("Nice!")
+        break
+
